8f2eecb137141535550176b3a961b3a5eb674cce
TedoHarisC <tedoharischandra29@gmail.com>
2022-05-23 10:54:43 +0700

    Menambahkan semua model yang dibutuhkan

---
+++ app/Models/Transaction.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'users_id',
+        'address',
+        'payment',
+        'total_price',
+        'shipping_price',
+        'status'
+    ];
+
+    public function user() 
+    {
+        return $this->belongsTo(User::class, 'users_id', 'id');
+    }
+
+    public function items()
+    {
+        return $this->hasMany(TransactionItem::class, 'transactions_id', 'id');
+    }
+}
