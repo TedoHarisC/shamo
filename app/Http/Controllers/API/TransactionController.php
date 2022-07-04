88fa0c06469807a03e112e9b5a19cbd58a0339b4
TedoHarisC <tedoharischandra29@gmail.com>
2022-07-04 07:58:23 +0700

    Membuat Checkout api

--- app/Http/Controllers/API/TransactionController.php
+++ app/Http/Controllers/API/TransactionController.php
@@ -44,4 +44,35 @@ class TransactionController extends Controller
             'Data list transaksi berhasil diambil'
         );
     }
+
+    public function checkout(Request $request)
+    {
+        $request->validate([
+            'items' => 'required|array',
+            'items.*.id' => 'exists:products, id',
+            'total_price' => 'required',
+            'shipping_price' => 'required',
+            'status' => 'required|in:PENDING,SUCCESS,CANCELLED,FAILED,SHIPPING,SHIPPED'
+        ]);
+
+        $transaction = Transaction::create([
+            'users_id' => Auth::user()->id,
+            'address' => $request->address,
+            'total_price' => $request->total_price,
+            'shipping_price' => $request->shipping_price,
+            'status' => $request->status,
+        ]);
+
+        foreach($request->items as $product) {
+            TransactionItem::create([
+                'users_id' => Auth::user()->id,
+                'products_id' => $product['id'],
+                'transactions_id' => $transaction->id,
+                'quantity' => $product['quantity'],
+            ]);
+        }
+
+        // Digunakan untuk melakukan load pada item product karena agar data update
+        return CustomResponseFormatter::success($transaction->load('items.product'), 'Transaksi berhasil');
+    }
 }
