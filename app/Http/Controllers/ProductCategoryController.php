3ea0348166c4d573b2b0c956a2a7016401f9231d
TedoHarisC <tedoharischandra29@gmail.com>
2022-11-30 10:37:17 +0700

    Menambahkan Halaman Admin Product Category

---
+++ app/Http/Controllers/ProductCategoryController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ProductCategoryRequest;
+use App\Models\ProductCategory;
+use Illuminate\Http\Request;
+use Yajra\DataTables\Facades\DataTables;
+
+class ProductCategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        if (request()->ajax()) {
+            $query = ProductCategory::query();
+
+            return DataTables::of($query)
+                ->addColumn('action', function ($item) {
+                    return '<a class="inline-block border border-gray-700 bg-gray-700 text-white rounded-md px-2 py-1 m-1 transition duration-500 ease select-none hover:bg-gray-800 focus:outline-none focus:shadow-outline" 
+                            href="' . route('dashboard.category.edit', $item->id) . '">
+                            Edit
+                        </a>';
+                })
+                ->rawColumns(['action'])
+                ->make();
+        }
+
+        return view('pages.dashboard.category.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(ProductCategoryRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ProductCategory  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ProductCategory $category)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ProductCategory  $productCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ProductCategory $category)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ProductCategory  $productCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function update(ProductCategoryRequest $request, ProductCategory $category)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ProductCategory  $productCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ProductCategory $category)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProductCategoryController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,16 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::group(['middleware' => ['auth:sanctum', 'verified']], function () {
+    Route::name('dashboard.')->prefix('dashboard')->group(function () {
+        Route::get('/', [DashboardController::class, 'index'])->name('index');
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified', 'admin'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+        Route::middleware(['admin'])->group(function () {
+            Route::resource('category', ProductCategoryController::class);
+        });
+    });
 });
